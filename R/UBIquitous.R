8d27bd7fb8cdc1a586f108768018e61b2d72a52c
Daniel Neves <dneves@kathmandu>
2018-03-21 12:36:43 +0000

    Added render_section

--- R/UBIquitous.R
+++ R/UBIquitous.R
@@ -354,4 +354,132 @@ render_document <- function(document, filename,
 
 
 
+#' Render a single section
+#'
+#' This function renders a section's HTML.
+#'
+#' @param section a section (may contain subsections)
+#' @param chunk_functions_ex functions used to render chunks (see details).
+#' @param section_functions functions used to render sections (a vector, defining the function to use at each section depth).
+#'
+#' @return
+#' @export
+#'
+#' @examples
+render_section <- function(section,
+                           chunk_functions_ex = NULL,
+                           section_functions = NULL,
+                           title="Untitled") {
+  require(knitr)
+  require(kableExtra)
+
+  number_sections=TRUE
+  debug=FALSE
+
+  if (is.null(section_functions)) {
+    section_functions <- c(render_section_basic,
+                           render_section_panel3,
+                           render_section_panel3,
+                           render_section_panel3,
+                           render_section_panel3)
+  }
+
+  # setup options
+  chunk_functions = list("table"=render_table_chunk,
+                         "text"=render_text_chunk,
+                         "figure"=render_figure_chunk,
+                         "file"=render_file_chunk)
+
+  for (n in names(chunk_functions_ex)) {
+    chunk_functions[[ n ]] <- chunk_functions_ex[[ n ]]
+  }
+
+  params <- list(author="Daniel Neves (dneves@igc.gulbenkian.pt)",
+                 chunk_functions = chunk_functions,
+                 section_functions = section_functions)
+
+  # process the chunks
+  process_chunks <- function(chunks, sec_path) { #parent_ns, sec_num, sec_depth) {
+    # set up a namespace for this module
+    ns <- paste(sec_path, collapse="_") # paste0(parent_ns, "_", sec_num)
+
+    # get the *markdown* (not html...) for each chunk
+    chunks_html <- paste(lapply(seq_along(chunks), function(i) {
+      x <- chunks[[i]]
+
+      paste('<div>',
+            chunk_functions[[ x$type ]](x, c(sec_path, i), params=params), #  i, ns, sec_depth+1),
+            '</div>',
+            sep="\n")
+    }), collapse="\n***\n") # TODO: set chunk separator as parameter
+
+    return(chunks_html)
+  }
+
+  # process a section
+  process_section <- function(section, sec_path) {
+    # setup namespace for this section
+    sec_num <- rev(sec_path)[1]
+    sec_depth <- length(sec_path)
+    ns <- paste(sec_path, collapse="_") # paste0(parent_ns, "_", sec_num)
+
+    hx <- paste0("h", sec_depth)
+
+    if (number_sections == TRUE & !is.null(section$title)) {
+      section$title <- paste(paste(sec_path, collapse="."), section$title) #paste0(gsub('_', '.', substring(ns, 3)), " ", section$title)
+    }
+
+    if (!is.null(section$description)) {
+      section_description <- paste0('<p>', section$description, '</p><hr/>')
+    } else {
+      section_description <- ""
+    }
+
+    # make the parameters panel
+    # if (!is.null(names(section$par)))
+    parsdf <- data.frame(Parameter=names(section$par), Value=sapply(section$par, toString))
+    if (nrow(parsdf) > 0) {
+      pars_html <- paste(
+        as.character(kable_styling(kable(parsdf, row.names = FALSE, format="html"), bootstrap_options = c("condensed"))),
+        sep="\n")
+    } else {
+      pars_html <- ""
+    }
+
+    chunks_html <- process_chunks(section$chunks, sec_path) #parent_ns = ns, sec_num = 0, sec_depth = sec_depth)
+
+    # process subsections
+    subsections_html <- paste(lapply(seq_along(section$subsections), function(i) {
+      process_section(section$subsections[[i]], c(sec_path, i))  #parent_ns=ns, sec_num = i, sec_depth = sec_depth+1)
+    }), collapse="\n")
+
+    section_body <- paste(chunks_html, subsections_html, sep="\n")
+
+    # this is the html for this section
+    if (!is.null(section$render_section))
+      render_func <- section$render_section
+    else
+      render_func <- section_functions[[ sec_depth ]]
+
+    out_html <- paste(render_func(ns, sec_depth, section$title, section_description, pars_html, section_body), "\n")
+  }
+
+  # process sections
+  # ns <- "0"
+
+  # chunks on the main section
+  # chunks_html <- process_chunks(document$chunks, 0) # parent_ns = ns, sec_num = 0, sec_depth = 0)
+
+  # process sections
+  section_html <- process_section(section, 0) # ns, i, 1)
+
+  # sections_html <- paste(lapply(seq_along(document$sections), function(i) {
+  #   process_section(document$sections[[i]], i) # ns, i, 1)
+  # }), collapse="\n")
+
+  out_html <- section_html
+
+  return(out_html)
+}
+
 
